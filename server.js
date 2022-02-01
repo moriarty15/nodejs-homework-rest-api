b1e2a921a6683df6c4d015a13526f90f19fd6c05
Vladimir Palamov <84524771+moriarty15@users.noreply.github.com>
2022-02-01 22:04:13 +0800

    first push hw3

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log(`Server running. Use our API on port: ${3000}`)
-})
+const mongoose = require('mongoose');
+
+const DB_HOST = "mongodb+srv://Vladimir:1151020Pp@cluster0.0fzje.mongodb.net/db-contacts?retryWrites=true&w=majority"
+
+mongoose.connect(DB_HOST)
+  .then(() => console.log("database connect success"))
+  .catch(error => console.log(error.message))
+
+// app.listen(3000, () => {
+//   console.log(`Server running. Use our API on port: ${3000}`)
+// })
